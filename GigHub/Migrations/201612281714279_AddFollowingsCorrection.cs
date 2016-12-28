5138196d4bffc5ec8732c6e76b21d0159bedb549
Timothy Lucas <tllucas1s@gmail.com>
2016-12-28 09:16:16 -0800

    Add Following Actions.

---
+++ GigHub/Migrations/201612281714279_AddFollowingsCorrection.cs
@@ -0,0 +1,26 @@
+namespace GigHub.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddFollowingsCorrection : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.Followings", name: "Followee_Id", newName: "FolloweeId");
+            RenameIndex(table: "dbo.Followings", name: "IX_Followee_Id", newName: "IX_FolloweeId");
+            DropPrimaryKey("dbo.Followings");
+            AddPrimaryKey("dbo.Followings", new[] { "FollowerId", "FolloweeId" });
+            DropColumn("dbo.Followings", "FollowedId");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Followings", "FollowedId", c => c.String(nullable: false, maxLength: 128));
+            DropPrimaryKey("dbo.Followings");
+            AddPrimaryKey("dbo.Followings", new[] { "FollowerId", "FollowedId" });
+            RenameIndex(table: "dbo.Followings", name: "IX_FolloweeId", newName: "IX_Followee_Id");
+            RenameColumn(table: "dbo.Followings", name: "FolloweeId", newName: "Followee_Id");
+        }
+    }
+}
